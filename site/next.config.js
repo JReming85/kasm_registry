8b4d354da0920bee36a65dc869ea7fcaadfb952d
Jeremy R <jeremyrem@gmail.com>
2024-01-15 00:49:15 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'JRem's Kasm Registry',
+    description: 'Repo for my kasm images',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/JReming85/kasm_registry/',
+    contactUrl: 'https://github.com/JReming85/kasm_registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
